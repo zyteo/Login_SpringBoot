a81c0052276cecba2f3418df46d9bfe474d5dd87
Stabilo <zteo015@e.ntu.edu.sg>
2021-12-19 13:31:51 +0800

    Done with usercontroller

--- login/src/main/java/com/springbootreact/login/controller/UserController.java
+++ login/src/main/java/com/springbootreact/login/controller/UserController.java
@@ -1,9 +1,9 @@
 package com.springbootreact.login.controller;
 
-import com.login.springbootlogin.payload.response.MessageResponse;
+
 import com.springbootreact.login.model.User;
 import com.springbootreact.login.repository.UserRepository;
-
+import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,8 +27,21 @@ public class UserController {
 
 //  get request for logging in - get user details for frontend to use
   @GetMapping("/user/{username}")
-  public ResponseEntity<Tutorial> getTutorialById(@PathVariable("id") String id) {
-    
+  public ResponseEntity<User> getTutorialById(@PathVariable("username") String username) {
+		    try {
+		    	Optional<User> userData = userRepository.findByUsername(username);
+
+//		    	if username doesnt exist
+		      if (userData.isEmpty()) {
+		        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		      }
+//		      else username exists, return the user
+		      return new ResponseEntity<>(userData.get(), HttpStatus.OK);
+//		      handle errors
+		    } catch (Exception e) {
+		      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		    }
+
   }
 
 //  post request for signing up - creating new user
@@ -38,7 +52,6 @@ public class UserController {
 //		  Not sure how to set message payload response, so I set as httpstatus 403 and 409.
 		  if (userRepository.existsByUsername(user.getUsername())) {
 			  return new ResponseEntity<>(null, HttpStatus.FORBIDDEN);
-				
 			}
 
 			if (userRepository.existsByName(user.getName())) {
@@ -48,18 +61,22 @@ public class UserController {
 //		  Create new user
 		    User _user = userRepository.save(new User(user.getName(), user.getUsername(), user.getPassword(), "User"));
 		    return new ResponseEntity<>(_user, HttpStatus.CREATED);
+//		    handle errors
 		  } catch (Exception e) {
 		    return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		  }
-	 
-
-
   }
   
 
 //  for deleting user
   @DeleteMapping("/user/{id}")
   public ResponseEntity<HttpStatus> deleteUser(@PathVariable("id") String id) {
-  }
+	    try {
+	      userRepository.deleteById(id);
+	      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	    } catch (Exception e) {
+	      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	    }
+	  }
 
 }
